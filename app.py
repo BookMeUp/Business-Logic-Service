ea1b087bc18fbf4ad8bba418bae12cd6e570c6ff
robert-nst <ionut.nastase.robert@gmail.com>
2025-04-25 11:11:31 +0300

    add prometheus metrics endpoint

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from prometheus_flask_exporter import PrometheusMetrics
 from routes import register_blueprints
 
 app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+
 app.config['JWT_SECRET_KEY'] = 'super-secret-key'  # same as in auth-service
 
 jwt = JWTManager(app)
@@ -10,4 +14,4 @@ jwt = JWTManager(app)
 register_blueprints(app)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=False)
